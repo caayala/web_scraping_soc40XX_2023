bb3233859e3d72379b4fae4e7d31f8f15b2b5429
Cristián Ayala <caayala@gmail.com>
2023-07-08 13:43:50 -0400

    Código de clase 11. GitHub Actions.

---
+++ slides/class_11/class_11_captura.R
@@ -0,0 +1,102 @@
+# title: "Clase 11: GitHub Actions"
+# subtitle: "Web Scraping y acceso a datos desde la web"
+# author: Cristián Ayala
+# date: 2023-07-11
+
+# Cargar paquetes. Solo los necesarios.
+
+library(rvest)
+
+url <- 'https://www.latercera.com/lo-ultimo/page/'
+
+# Función para obtener elementos de interés de cada artículo.
+fun_parse_articulos <- function(.html){
+  # Captura de artículos dentro de la sección de clase .top-mainy
+  html_art <- .html |> 
+    html_elements('.top-mainy article')
+  
+  html_headline <- html_art |> 
+    html_element('.headline')
+  
+  list(
+    titulo      = html_headline |> html_text(),
+    url_noticia = html_headline |> html_element('a') |> html_attr('href'),
+    tag         = html_headline |> html_element('.tag') |> html_text(),
+    texto       = html_art |> html_element('.deck p') |> html_text(),
+    url_imagen  = html_art |> html_element('img') |> html_attr('src'),
+    periodista  = html_art |> html_element('.byline') |> html_text(),
+    tiempo      = html_art |> html_element('.time') |> html_text()
+  ) |> 
+    as.data.frame()
+}
+
+
+# Prueba de página 1 ----
+
+if(FALSE){
+  # Lectura de sitio web
+  html_1 <- read_html(paste0(url, '1'))
+  
+  df_1 <- fun_parse_articulos(html_1) 
+}
+
+
+# Lectura de 10 páginas ----
+
+l_url <- paste0(url, 1:10)
+
+# Lectura de las 10 páginas.
+l_html <- lapply(l_url, 
+                 function(.url){
+                   Sys.sleep(1) # Pausa de 1 segundo
+                   read_html(.url)
+                 })
+
+
+# Procesamiento ----
+l_contenido <- lapply(l_html,
+                      fun_parse_articulos)
+
+# Generar solo una tabla.
+df_contenido <- do.call(rbind, 
+                        l_contenido)
+
+
+# Correcciones ----
+
+# Remover texto contenido dentro de nodos.
+fun_remove <- function(contenido, texto_a_rm){
+  lapply(seq_along(contenido), 
+         \(i) sub(texto_a_rm[[i]], "", contenido[[i]])
+  ) |> 
+    unlist()
+}
+
+# Quitar texto de tiempo en el campo de periodistas.
+df_contenido$periodista <- fun_remove(df_contenido$periodista, df_contenido$tiempo)
+# Quitar texto de tag en el campo de título.
+df_contenido$titulo <- fun_remove(df_contenido$titulo, df_contenido$tag)
+
+# Fecha de captura.
+df_contenido$fecha_captura <- format(Sys.Date(), '%Y-%m-%d')
+
+
+# Grabación ----
+
+# Guardar datos en archivo de texto.
+# Se agregan al final del archivo creado en la primera captura de información.
+write.table(df_contenido,
+            file = 'slides/class_11/class_11_files/df_contenido.csv',
+            quote = TRUE,
+            row.names = FALSE,
+            qmethod = 'double',
+            fileEncoding = "utf8",
+            sep = ';',
+            dec = ',',
+            append = TRUE)
+
+# Para leer archivo
+# df_test <- read.csv('slides/class_11/class_11_files/df_contenido.csv',
+#                     header = TRUE,
+#                     sep = ';') |> 
+#   tibble::as_tibble()
